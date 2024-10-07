6dcb6e971d9983dba5d576f87472f903cbd72dec
darthun <62441274+darthun@users.noreply.github.com>
2024-10-06 22:38:05 -0400

    commented the import to satisfy the picky ES rule

--- ugly-butter-site/src/app/login/page.tsx
+++ ugly-butter-site/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+//import { useRouter } from 'next/navigation'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
 export default function LoginPage() {
